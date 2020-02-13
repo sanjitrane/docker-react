b60fda94db220a1fce9909efc538a5cea7add6da
sanjit <sanjit@NOVI0751.local>
2020-02-13 20:05:51 +0530

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Changed</p>
+        <p>Docker React Test</p>
         <a
           className='App-link'
           href='https://reactjs.org'
